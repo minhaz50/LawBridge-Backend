0104bdd98c5f6a198740637be6ccb8cd890d7358
Md Minhaz Uddin <md.minhazuddin2597@gmail.com>
2026-08-17 13:17:09 +0600

    Google auth client setup for google authentication.

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -9,4 +9,12 @@ export default {
   node_env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  backend_url: process.env.BACKEND_URL,
+  frontend_url: process.env.FRONTEND_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET!,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
+  google_client_id: process.env.GOOGLE_CLIENT_ID,
 };
